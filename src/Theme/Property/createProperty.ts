2b6d4bc50db6377bcdb224287689e81cf2f4fda1
Dawid Wojda <dawid@code8.pl>
2022-01-13 22:32:51 +0100

    fix property name consistency if it was written in camel case

--- src/Theme/Property/createProperty.ts
+++ src/Theme/Property/createProperty.ts
@@ -1,7 +1,8 @@
 import type { IProperty } from '../../../types/IProperty';
 import type { IDefaultVariant } from '../../../types/IVariantsMap';
 import { Property } from './Property';
+import { camelToKebabCase } from '../../common';
 
 export function createProperty<T extends IDefaultVariant>(name: string, variants: T): IProperty<T> {
-    return new Property<T>(name, variants);
+    return new Property<T>(camelToKebabCase(name), variants);
 }
